5185416b5dfcfff3f7bf4906618ab2196b23cef2
Carl Hosteler <carl.hostetler@cdw.com>
2025-03-06 11:39:48 -0800

    deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://choste.github.io',
+	base: 'family-history',
 	integrations: [
 		starlight({
 			title: 'Family History',
